7d7e5a2b2e1d52e6948c3bb52932ca8bfefecbd3
Adam Bradbury <f.adambrad@gmail.com>
2017-09-25 13:50:24 -0600

    Through Excersize 8

--- drills.js
+++ drills.js
@@ -44,6 +44,50 @@
   
 // whoAmI('Chris', 29);
 
+//Excersize 6
+
+// function yearOfBirth(age) {
+//   try {
+//     if (age < 0) throw new Error('Age can not be negative');
+//   }
+//   catch (err) {
+//     console.log(err);
+//   }
+//   return 2016 - age;
+// }
+
+// function whoAmI(name, age) {
+//   var yob = yearOfBirth(age);
+//   console.log('Hi, my name is ' + name + ' and I\'m ' + age + ' years old.');
+//   console.log('I was born in ' + yob + ' .');
+// }
+   
+// whoAmI('Chris', -29);
+
+
+//Excersize 7 
+
+// function yearOfBirth(age) {
+//   try {
+//     if (age < 0) throw new Error('Age can not be negative');
+//   }
+//   catch (err) {
+//     console.log(err);
+//   }
+//   return 2016 - age;
+// }
+  
+// function whoAmI(name, age) {
+//   if (!name || !age) {console.error('Arguments not valid');}
+//   var yob = yearOfBirth(age);
+//   console.log('Hi, my name is ' + name + ' and I\'m ' + age + ' years old.');
+//   console.log('I was born in ' + yob + ' .');
+// }
+     
+// whoAmI();
+
+//Excersize 8
+  
 function yearOfBirth(age) {
   try {
     if (age < 0) throw new Error('Age can not be negative');
@@ -53,16 +97,20 @@ function yearOfBirth(age) {
   }
   return 2016 - age;
 }
-
+    
 function whoAmI(name, age) {
-  var yob = yearOfBirth(age);
+  if (!name || name !== 'string' || !age || isNaN(age)) {console.error('Arguments not valid');}
+  const yob = yearOfBirth(age);
   console.log('Hi, my name is ' + name + ' and I\'m ' + age + ' years old.');
   console.log('I was born in ' + yob + ' .');
 }
-   
-whoAmI('Chris', -29);
-
-
+       
+whoAmI('Chris', 28);
+  
+  
+  
+  
+  
 
 
 
