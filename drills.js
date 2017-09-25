03e1350d9619e66fff8c781dcf9a1f0739fff2a5
Onesun15 <sunnysharma15@hotmail.com>
2017-09-25 14:47:24 -0400

    added 2 exercises completed

---
+++ drills.js
@@ -0,0 +1,15 @@
+'use strict';
+// Exercis 1
+console.log('Hi, my name is Chris and I\'m 29 years old');
+
+
+// Exercise 2
+function whoAmI() {
+  console.log('Hi, my name is Chris and I\'m 29 years old');
+
+}
+
+whoAmI();
+
+// Exercise 3
+  
