f0e331471443e380a241121002d4ffbea1b0094c
Onesun15 <sunnysharma15@hotmail.com>
2017-09-25 15:19:26 -0400

    completed upto exercise 6

--- drills.js
+++ drills.js
@@ -22,11 +22,47 @@
 
 // Excercise 4
 
-function whoAmI(name, age) {
-  const yearOfBirth = 2016 - age;
-  console.log('Hi, my name is ' + name + ' and I\'m ' + age + ' years old.');
-  console.log('I was born in ' + yearOfBirth + ' .');
-}
+// function whoAmI(name, age) {
+//   var yearOfBirth = 2016 - age;
+//   console.log('Hi, my name is ' + name + ' and I\'m ' + age + ' years old.');
+//   console.log('I was born in ' + yearOfBirth + ' .');
+// }
+    
+// whoAmI('Chris', 29);
+
+// Exercise 5
+
+// function yearOfBirth(age) {
+//   return 2016 - age;
+// }
+
+// function whoAmI(name, age) {
+//   var yob = yearOfBirth(age);
+//   console.log('Hi, my name is ' + name + ' and I\'m ' + age + ' years old.');
+//   console.log('I was born in ' + yob + ' .');
+// }
   
-whoAmI('Chris', 29);
+// whoAmI('Chris', 29);
+
+function yearOfBirth(age) {
+  try {
+    if (age < 0) throw new Error('Age can not be negative');
+  }
+  catch (err) {
+    console.log(err);
+  }
+  return 2016 - age;
+}
+
+function whoAmI(name, age) {
+  var yob = yearOfBirth(age);
+  console.log('Hi, my name is ' + name + ' and I\'m ' + age + ' years old.');
+  console.log('I was born in ' + yob + ' .');
+}
+   
+whoAmI('Chris', -29);
+
+
+
+
 
