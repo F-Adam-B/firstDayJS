3adfa14d867bc2c358f1ef45a16337322b9b86e1
Adam Bradbury <f.adambrad@gmail.com>
2017-09-25 13:01:06 -0600

    updated through excersize 4

--- drills.js
+++ drills.js
@@ -1,15 +1,32 @@
 'use strict';
-// Exercis 1
-console.log('Hi, my name is Chris and I\'m 29 years old');
+// // Exercis 1
+// console.log('Hi, my name is Chris and I\'m 29 years old');
 
 
-// Exercise 2
-function whoAmI() {
-  console.log('Hi, my name is Chris and I\'m 29 years old');
+// // Exercise 2
+// function whoAmI() {
+//   console.log('Hi, my name is Chris and I\'m 29 years old');
 
-}
+// }
 
-whoAmI();
+// whoAmI();
 
-// Exercise 3
+// // Exercise 3
+
+// function whoAmI(name, age) {
+//     console.log('Hi, my name is Chris and I\'m 29 years old');
   
+//   }
+  
+//   whoAmI();
+
+// Excercise 4
+
+function whoAmI(name, age) {
+  const yearOfBirth = 2016 - age;
+  console.log('Hi, my name is ' + name + ' and I\'m ' + age + ' years old.');
+  console.log('I was born in ' + yearOfBirth + ' .');
+}
+  
+whoAmI('Chris', 29);
+
